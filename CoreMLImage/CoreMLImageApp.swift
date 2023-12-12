d8724611d9fd7702069ef5f5916c6439916ebd85
Freddy <fredericdouamba2014@gmail.com>
2023-12-11 23:33:06 -0800

    Initial Commit

---
+++ CoreMLImage/CoreMLImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLImageApp.swift
+//  CoreMLImage
+//
+//  Created by Student Account on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
